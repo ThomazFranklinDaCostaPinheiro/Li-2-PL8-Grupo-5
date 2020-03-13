4f84fe7919c99eed074e4860fbb0b9a7e1fda038
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-03-13 16:08:20 +0000

    Update Lógica.c

--- Trabalho de Li/Lógica.c
+++ Trabalho de Li/Lógica.c
@@ -3,8 +3,20 @@
 //
 
 #include "Lógica.h"
+#include "Dados.h"
+#include <stdio.h>
 
 int jogar(ESTADO *e, COORDENADA c) {
     printf("jogar %d %d\n", c.coluna, c.linha);
+    int xu;
+    int yu;
+    xu = e->ultima_jogada.coluna;
+    yu = e->ultima_jogada.linha;
+    e->tab[xu][yu] = PRETA;
+    int l;
+    int c;
+    l = c.linha;
+    c = c.coluna;
+    e->tab[c][l] = BRANCA;
     return 1;
 }
