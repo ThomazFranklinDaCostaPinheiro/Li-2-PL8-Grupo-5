8de9305913cb0f937f254410df218ed43c10fc32
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-03-13 15:55:09 +0000

    Update Dados.c

--- Trabalho de Li/Dados.c
+++ Trabalho de Li/Dados.c
@@ -47,8 +47,8 @@ int obter_numero_de_jogadas(ESTADO *estado){
 CASA obter_estado_casa(ESTADO *e, COORDENADA c){
     int a;
     int b;
-    b = c.linha;
-    a = c.coluna;
+    b = e.linha;
+    a = e.coluna;
     CASA casa;
     casa = tab[a][b];
     return casa;
