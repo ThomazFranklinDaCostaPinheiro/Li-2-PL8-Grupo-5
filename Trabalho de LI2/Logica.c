15ee69632aeb9d06a640df43bfc3fc12d920587a
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-03-19 20:46:46 +0000

    Update Logica.c

--- Trabalho de LI2/Logica.c
+++ Trabalho de LI2/Logica.c
@@ -5,6 +5,39 @@
 #include "Logica.h"
 #include "Dados.h"
 #include <stdio.h>
+#include <math.h>
+
+int e_vizinho (COORDENADA c1, COORDENADA c2){
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+    x1 = c1.coluna;
+    y1 = c1.linha;
+    x2 = c2.coluna;
+    y2 = c2.linha;
+    if (abs(x1-x2)<= 1){
+        if (abs(y1-y2)<= 1)
+            return 1;
+        else
+            return 0;
+    }
+    else
+        return 0;
+}
+
+int e_vazio (COORDENADA c3, ESTADO* state){
+    int x3;
+    int y3;
+    x3 = c3.coluna;
+    y3 = c3.linha;
+    if (state->tab[x3][y3]==VAZIO)
+        return 1;
+    else
+        return 0;
+}
+
+
 
 int jogar(ESTADO *e, COORDENADA c) {
     printf("jogar %d %d\n", c.coluna, c.linha);
@@ -12,11 +45,20 @@ int jogar(ESTADO *e, COORDENADA c) {
     int yu;
     xu = e->ultima_jogada.coluna;
     yu = e->ultima_jogada.linha;
-    e->tab[xu][yu] = PRETA;
     int l;
     int co;
     l = c.linha;
     co = c.coluna;
-    e->tab[co][l] = BRANCA;
+    if ((e_vazio(c,e))&&(e_vizinho(c,e->ultima_jogada))){
+        e->tab[yu][xu] = PRETA;
+        e->tab[l][co] = BRANCA;
+        e->ultima_jogada.linha = l;
+        e->ultima_jogada.coluna = co;
+        e->num_jogadas= e->num_jogadas+1;
+    }
+    else{
+        printf("Jogada Invalida\n");
+        e->tab[yu][xu] = BRANCA;
+    }
     return 1;
 }
