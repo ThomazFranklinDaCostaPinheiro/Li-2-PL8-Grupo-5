020e810d531d8b2730d7ef776630ce1a2b855964
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-03-20 12:30:12 +0000

    Update Logica.h

--- Trabalho de LI2/Logica.h
+++ Trabalho de LI2/Logica.h
@@ -5,5 +5,12 @@
 #ifndef TRABALHO_DE_LI_LOGICA_H
 #define TRABALHO_DE_LI_LOGICA_H
 
+#include "Dados.h"
+#include "Interface.h"
+
+int e_vizinho (COORDENADA c1, COORDENADA c2);
+int e_vazio (COORDENADA c3, ESTADO* state);
+int e_peca (COORDENADA c);
+int jogar(ESTADO *e, COORDENADA c);
 
 #endif //TRABALHO_DE_LI_LOGICA_H
