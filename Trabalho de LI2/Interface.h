193f93835028beeb2f7169804a624acdca226f82
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-03-22 17:52:00 +0000

    Update Interface.h

--- Trabalho de LI2/Interface.h
+++ Trabalho de LI2/Interface.h
@@ -9,6 +9,9 @@
 #include "Logica.h"
 
 int quem_ganha(ESTADO *estado);
+int encurralado(ESTADO *estado);
+int gravar(ESTADO *estado, char filename[]);
+int lerfich(char filename[], ESTADO *estado;
 int vencer(ESTADO *estado);
 void mostrar_tabuleiro (ESTADO *estado);
 int interpretador(ESTADO *e);
