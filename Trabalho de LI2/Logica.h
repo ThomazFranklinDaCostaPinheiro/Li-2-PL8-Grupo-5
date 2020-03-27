7e83797b3644cc821af9b79679c8f0dde4122de5
ThomazFranklinDaCostaPinheiro <62022403+ThomazFranklinDaCostaPinheiro@users.noreply.github.com>
2020-03-27 12:40:07 +0000

    Update Logica.h

--- Trabalho de LI2/Logica.h
+++ Trabalho de LI2/Logica.h
@@ -8,9 +8,16 @@
 #include "Dados.h"
 #include "Interface.h"
 
+/**Função que verifica se duas peças são vizinhas.*/
 int e_vizinho (COORDENADA c1, COORDENADA c2);
+
+/** Essa função verifica se a casa em que se pretende jogar está vazia ou não.*/
 int e_vazio (COORDENADA c3, ESTADO* state);
+
+/** Função que verifica se a coordenada pertence ao tabuleiro.*/
 int e_peca (COORDENADA c);
+
+/**Função que realiza a jogada.*/
 int jogar(ESTADO *e, COORDENADA c);
 
 #endif //TRABALHO_DE_LI_LOGICA_H
