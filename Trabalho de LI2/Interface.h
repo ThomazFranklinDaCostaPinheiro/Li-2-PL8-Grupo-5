d479d5f64ce7178c6e0536e53fc9c175f38e309c
ThomazFranklinDaCostaPinheiro <62022403+ThomazFranklinDaCostaPinheiro@users.noreply.github.com>
2020-03-27 12:37:50 +0000

    Update Interface.h

--- Trabalho de LI2/Interface.h
+++ Trabalho de LI2/Interface.h
@@ -8,12 +8,25 @@
 #include "Dados.h"
 #include "Logica.h"
 
+/** Essa função determina quem foi o vencedor do jogo.*/
 int quem_ganha(ESTADO *estado);
+
+/** Essa função indica se um jogador está encurralado, ou seja, sem possibilidades de jogar.*/
 int encurralado(ESTADO *estado);
+
+/** Essa função cria um novo ficheiro onde é gravado o estado do jogo.*/
 int gravar(ESTADO *estado, char filename[]);
+
+/** Essa função basicamente lê o ficheiro.*/
 int lerfich(char filename[], ESTADO *estado);
+
+/** Essa função determina se um jogador venceu.*/
 int vencer(ESTADO *estado);
+
+/* Essa função mostra o tabuleiro atualizado.*/
 void mostrar_tabuleiro (ESTADO *estado);
+
+/** Essa função interpreta os comandos do utilizador.*/
 int interpretador(ESTADO *e);
 
 #endif //TRABALHO_DE_LI_INTERFACE_H
