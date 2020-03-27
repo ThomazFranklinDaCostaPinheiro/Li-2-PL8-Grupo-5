6b15b5f444683d9f468a2ced130e417c62f90fb6
ThomazFranklinDaCostaPinheiro <62022403+ThomazFranklinDaCostaPinheiro@users.noreply.github.com>
2020-03-27 12:29:08 +0000

    Update Dados.h

--- Trabalho de LI2/Dados.h
+++ Trabalho de LI2/Dados.h
@@ -26,10 +26,17 @@ typedef struct {
     int jogador_atual;
 } ESTADO;
 
+/**Essa função inicia o tabuleiro.*/
 
 ESTADO *inicializar_estado();
+
+
 int obter_jogador_atual(ESTADO *estado);
+
+
 int obter_numero_de_jogadas(ESTADO *estado);
+
+
 CASA obter_estado_casa(ESTADO *e, COORDENADA c);
 
 
