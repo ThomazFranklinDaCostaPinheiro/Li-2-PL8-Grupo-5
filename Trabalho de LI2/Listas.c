3864aab55c39041c17d4f6a181329831e1db7119
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-04-19 14:38:08 +0100

    Add files via upload

---
+++ Trabalho de LI2/Listas.c
@@ -0,0 +1,41 @@
+//
+// Created by danie on 18/04/2020.
+//
+
+#include "Listas.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+LISTA criar_lista(){
+    LISTA r = malloc(sizeof(NODO));
+    r->proximo = NULL;
+    return r;
+}
+
+LISTA insere_cabeca(LISTA L, void *valor){
+    LISTA r = malloc(sizeof(NODO));
+    r->valor = valor;
+    r->proximo = L;
+}
+
+void *devolve_cabeca(LISTA L){
+    return (L->valor);
+}
+
+LISTA proximo(LISTA L){
+    return (L->proximo);
+}
+
+LISTA remove_cabeca(LISTA L){
+    LISTA temp = criar_lista();
+    temp = L->proximo;
+    free(L);
+    return temp;
+}
+
+int lista_esta_vazia(LISTA L){
+    if(L == NULL)
+        return 1;
+    else
+        return 0;
+}
