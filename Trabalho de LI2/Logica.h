9d641573f88418f188bc78fea26454ef78d52f2c
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-04-25 16:11:29 +0100

    Update Logica.h

--- Trabalho de LI2/Logica.h
+++ Trabalho de LI2/Logica.h
@@ -9,10 +9,14 @@
 #include "Interface.h"
 #include "Listas.h"
 
+/** Essa função determina quem foi o vencedor do jogo.*/
 int quem_ganha(ESTADO *estado);
+/** Essa função indica se um jogador está encurralado, ou seja, sem possibilidades de jogar.*/
 int encurralado(ESTADO *estado);
 /**Função que verifica se duas peças são vizinhas.*/
 int e_vizinho (COORDENADA c1, COORDENADA c2);
+/** Essa função determina se um jogador venceu.*/
+int vencer(ESTADO *estado);
 /** Essa função verifica se a casa em que se pretende jogar está vazia ou não.*/
 int e_vazio (COORDENADA c3, ESTADO* state);
 /** Função que verifica se a coordenada pertence ao tabuleiro.*/
@@ -23,15 +27,19 @@ int jogar(ESTADO *e, COORDENADA c);
 int movs(ESTADO *e);
 /** Essa função basicamente nos dá a coordenada atual.*/
 int write_coord(COORDENADA coord);
-
+/** Esta função permite mover o jogo para qualquer uma das suas fases. */
 ESTADO *pos (ESTADO *e, int i);
+/** Função que limpa o tabuleiro */
 ESTADO * reset_tab (ESTADO *e);
+/** Desenha no tabuleiro uma sequência dois movimentos */
 int *desenha_jogada(ESTADO *e, JOGADA jogada);
-ESTADO *pos (ESTADO *e, int i);
+/** Função que devolve uma coordenada a que foi adicionad os valores x e y */
 COORDENADA *offset(COORDENADA c, int x, int y);
+/** Cria uma lista das casas disponiveis em torno da peça branca */
 LISTA casas_disp(ESTADO *e);
+/** Devolve uma coordenada aleatória */
 COORDENADA rand_coord(LISTA l);
+/** Realiza uma jogada segundo um algoritmo */
 int jogs(ESTADO *e);
-int vencer(ESTADO *estado);
 
 #endif //TRABALHO_DE_LI_LOGICA_H
