ba896a4cb7a3da6d89de9d64daf8200e4edbb3aa
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-04-25 16:02:16 +0100

    Update Logica.h

--- Trabalho de LI2/Logica.h
+++ Trabalho de LI2/Logica.h
@@ -7,34 +7,31 @@
 
 #include "Dados.h"
 #include "Interface.h"
+#include "Listas.h"
 
+int quem_ganha(ESTADO *estado);
+int encurralado(ESTADO *estado);
 /**Função que verifica se duas peças são vizinhas.*/
 int e_vizinho (COORDENADA c1, COORDENADA c2);
-
 /** Essa função verifica se a casa em que se pretende jogar está vazia ou não.*/
 int e_vazio (COORDENADA c3, ESTADO* state);
-
 /** Função que verifica se a coordenada pertence ao tabuleiro.*/
 int e_peca (COORDENADA c);
-
 /**Função que realiza a jogada.*/
 int jogar(ESTADO *e, COORDENADA c);
-
-char conv_c(int col);
-char conv_l(int lin);
-
-/** Essa função basicamente nos dá a coordenada atual.*/
-int write_coord(COORDENADA coord);
-
 /** Essa função nos dá os movimentos realizados até o momento. */
 int movs(ESTADO *e);
+/** Essa função basicamente nos dá a coordenada atual.*/
+int write_coord(COORDENADA coord);
 
 ESTADO *pos (ESTADO *e, int i);
 ESTADO * reset_tab (ESTADO *e);
 int *desenha_jogada(ESTADO *e, JOGADA jogada);
+ESTADO *pos (ESTADO *e, int i);
 COORDENADA *offset(COORDENADA c, int x, int y);
 LISTA casas_disp(ESTADO *e);
 COORDENADA rand_coord(LISTA l);
 int jogs(ESTADO *e);
+int vencer(ESTADO *estado);
 
 #endif //TRABALHO_DE_LI_LOGICA_H
