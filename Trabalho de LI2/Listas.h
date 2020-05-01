7338023e10c060b4a9f8ba87dbbf83894909ece4
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-05-01 19:07:14 +0100

    Update Listas.h

--- Trabalho de LI2/Listas.h
+++ Trabalho de LI2/Listas.h
@@ -10,17 +10,17 @@ typedef struct nodo {
     struct nodo *proximo;
 } NODO, *LISTA;
 
-// Cria uma lista vazia
+/** Cria uma lista vazia */
 LISTA criar_lista();
-// Insere um valor na cabeça da lista
+/** Insere um valor na cabeça da lista */
 LISTA insere_cabeca(LISTA L, void *valor);
-// Devolve a cabeça da lista
+/** Devolve a cabeça da lista */
 void *devolve_cabeca(LISTA L);
-// Devolve a cauda da lista
+/** Devolve a cauda da lista */
 LISTA proximo(LISTA L);
-// Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
+/** Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda */
 LISTA remove_cabeca(LISTA L);
-// Devolve verdareiro se a lista é vazia
+/** Devolve verdareiro se a lista é vazia */
 int lista_esta_vazia(LISTA L);
 
 #endif //TRABALHO_DE_LI2_LISTAS_H
