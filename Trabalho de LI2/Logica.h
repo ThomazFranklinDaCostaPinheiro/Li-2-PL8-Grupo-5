e18d93cc1753dea74758245c58a864e6cf548de1
ThomazFranklinDaCostaPinheiro <62022403+ThomazFranklinDaCostaPinheiro@users.noreply.github.com>
2020-05-02 15:04:47 +0100

    Update Logica.h

--- Trabalho de LI2/Logica.h
+++ Trabalho de LI2/Logica.h
@@ -1,6 +1,7 @@
-//
-// Created by Thomaz on 12/03/2020.
-//
+/**
+@file Logica.h
+Essa camada é responsável pela lógica por tras do jogo.
+ */
 
 #ifndef TRABALHO_DE_LI_LOGICA_H
 #define TRABALHO_DE_LI_LOGICA_H
@@ -9,41 +10,77 @@
 #include "Interface.h"
 #include "Listas.h"
 
-/** Essa função determina quem foi o vencedor do jogo.*/
+/**
+\brief Essa função determina quem foi o vencedor do jogo.
+*/
 int quem_ganha(ESTADO *estado);
-/** Essa função indica se um jogador está encurralado, ou seja, sem possibilidades de jogar.*/
+/**
+\brief Essa função indica se um jogador está encurralado, ou seja, sem possibilidades de jogar.
+*/
 int encurralado(ESTADO *estado);
-/**Função que verifica se duas peças são vizinhas.*/
+/**
+\brief Função que verifica se duas peças são vizinhas.
+*/
 int e_vizinho (COORDENADA c1, COORDENADA c2);
-/** Essa função determina se um jogador venceu.*/
+/** 
+\brief Essa função determina se um jogador venceu.
+*/
 int vencer(ESTADO *estado);
-/** Essa função verifica se a casa em que se pretende jogar está vazia ou não.*/
+/**
+\brief Essa função verifica se a casa em que se pretende jogar está vazia ou não.
+*/
 int e_vazio (COORDENADA c3, ESTADO* state);
-/** Função que verifica se a coordenada pertence ao tabuleiro.*/
+/**
+\brief Função que verifica se a coordenada pertence ao tabuleiro.
+*/
 int e_peca (COORDENADA c);
-/**Função que realiza a jogada.*/
+/**
+\brief Função que realiza a jogada.
+*/
 int jogar(ESTADO *e, COORDENADA c);
-/** Essa função nos dá os movimentos realizados até o momento. */
+/**
+\brief Essa função nos dá os movimentos realizados até o momento.
+*/
 int movs(ESTADO *e);
-/** Essa função basicamente nos dá a coordenada atual.*/
+/**
+\brief Essa função basicamente nos dá a coordenada atual.
+*/
 int write_coord(COORDENADA coord);
-/** Esta função permite mover o jogo para qualquer uma das suas fases. */
+/**
+\brief Esta função permite mover o jogo para qualquer uma das suas fases.
+*/
 ESTADO *pos (ESTADO *e, int i);
-/** Função que limpa o tabuleiro */
+/**
+\brief Função que limpa o tabuleiro
+*/
 ESTADO * reset_tab (ESTADO *e);
-/** Desenha no tabuleiro uma sequência dois movimentos */
+/** 
+\brief Desenha no tabuleiro uma sequência dois movimentos 
+*/
 int *desenha_jogada(ESTADO *e, JOGADA jogada);
-/** Cria uma lista das casas disponiveis em torno da peça branca */
+/**
+\brief Cria uma lista das casas disponiveis em torno da peça branca 
+*/
 LISTA casas_disp(ESTADO *e);
-/** Devolve uma coordenada aleatória */
+/**
+\brief Devolve uma coordenada aleatória
+*/
 COORDENADA rand_coord(LISTA l);
-/** Realiza uma jogada segundo um algoritmo aleatório*/
+/** 
+\brief Realiza uma jogada segundo um algoritmo aleatório
+*/
 int jogs(ESTADO *e);
-/** Devolve uma coordenada segundo a heuristica da distancia euclidiana. */
+/** 
+\brief Devolve uma coordenada segundo a heuristica da distancia euclidiana.
+*/
 COORDENADA euclidiana(LISTA l, int player);
-/** Calcula a distancia entre dois pontos. */
+/**
+\brief Calcula a distancia entre dois pontos. 
+*/
 double distancia(COORDENADA c1, COORDENADA c2);
-/** Realiza uma jogada segundo um algoritmo de distância euclidiana. */
+/**
+\brief Realiza uma jogada segundo um algoritmo de distância euclidiana.
+*/
 int jogs2(ESTADO *e);
 
 #endif //TRABALHO_DE_LI_LOGICA_H
