d06b51ba8c2367a90f815c5a575d7ca9261c41a0
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-05-03 11:58:08 +0100

    Update main.c

--- Bot/main.c
+++ Bot/main.c
@@ -4,12 +4,15 @@
 
 
 int main(int argc, char**argv){
-    ESTADO *e = inicializar_estado();
-    lerfich(argv[1],e);
-    while(vencer(e)){
-        jogs2(e);
-    }
-    printf("\nO vencedor da partida e o Jogador%d", quem_ganha(e));
-    gravar(e,argv[2]);
-    return 0;
+    if(argc == 3){
+        ESTADO *e = inicializar_estado();
+        lerfich(argv[1],e);
+        if(vencer(e)){
+            jogs2(e);
+            gravar(e,argv[2]);
+            return 0;
+        } else
+            printf("Player%2d venceu",quem_ganha(e));
+    } else
+        printf("Argumentos insuficientes.");
 }
