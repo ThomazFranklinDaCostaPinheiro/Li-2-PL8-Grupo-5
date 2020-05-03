b23379ee248b80499246282bb876f23bbbcee2e9
ThomazFranklinDaCostaPinheiro <62022403+ThomazFranklinDaCostaPinheiro@users.noreply.github.com>
2020-05-03 16:36:42 +0100

    Update Listas.h

--- projeto/Listas.h
+++ projeto/Listas.h
@@ -12,27 +12,38 @@ typedef struct nodo {
 } NODO, *LISTA;
 
 /**
-\brief Cria uma lista vazia
+\brief Cria uma lista vazia.
+@returns Retorna uma lista vazia
 */
 LISTA criar_lista();
 /** 
 \brief Insere um valor na cabeça da lista
+@param L É uma lista ligada
+@param valor É o valor a ser inserido na cabeça.
+@returns Retorna a lista com a cabeça inserida.
 */
 LISTA insere_cabeca(LISTA L, void *valor);
 /**
 \brief Devolve a cabeça da lista 
+@param L É uma lista ligada.
 */
 void *devolve_cabeca(LISTA L);
 /**
 \brief Devolve a cauda da lista
+@param L É uma lista ligada.
+@returns Retorna o resto da lista sem a cabeça.
 */
 LISTA proximo(LISTA L);
 /**
 \brief Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
+@param L É uma lista ligada
+@returns Retorna a lista sem a cabeça.
 */
 LISTA remove_cabeca(LISTA L);
 /**
 \brief Devolve verdareiro se a lista é vazia 
+@param L
+@returns Retorna 1, se a lista estiver vazia e 0 se não estiver vazia.
 */
 int lista_esta_vazia(LISTA L);
 
