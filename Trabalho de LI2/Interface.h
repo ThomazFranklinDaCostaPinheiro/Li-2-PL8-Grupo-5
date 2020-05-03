d015b05953d3086f089440bbd79c2aa610160469
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-05-03 01:14:45 +0100

    Update Interface.h

--- Trabalho de LI2/Interface.h
+++ Trabalho de LI2/Interface.h
@@ -18,6 +18,16 @@ int gravar(ESTADO *estado, char filename[]);
 */
 int lerfich(char filename[], ESTADO *estado);
 /**
+\brief Essa função basicamente nos dá a coordenada atual.
+@param coord É uma coordenada
+*/
+int write_coord(COORDENADA coord);
+/**
+\brief Essa função nos dá os movimentos realizados até o momento.
+@param e É o estado do jogo
+*/
+int movs(ESTADO *e);
+/**
 \brief Essa função mostra o tabuleiro atualizado.
 */
 void mostrar_tabuleiro (ESTADO *estado);
@@ -25,7 +35,7 @@ void mostrar_tabuleiro (ESTADO *estado);
 \brief Essa função interpreta os comandos do utilizador.
 */
 int interpretador(ESTADO *e);
-int char_int(char c);
+void erros(int n);
 
 
 #endif //TRABALHO_DE_LI_INTERFACE_H
