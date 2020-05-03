9f6a0f657a2cdc4802cdbc7b25206a184233e4f0
DanielConstantinoMartinsFaria <62064509+DanielConstantinoMartinsFaria@users.noreply.github.com>
2020-05-03 01:15:31 +0100

    Update Logica.c

--- Trabalho de LI2/Logica.c
+++ Trabalho de LI2/Logica.c
@@ -3,40 +3,32 @@
 //
 
 #include "Dados.h"
-#include "Logica.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 #include "Listas.h"
+#include "Interface.h"
+#include <math.h>
+
+
 
 int quem_ganha(ESTADO *estado){
-    if ((obter_linha_ult(estado) == 0)&&(obter_coluna_ult(estado) == 0))
+    if (((obter_linha_ult(estado) == 0)&&(obter_coluna_ult(estado) == 0)) || (obter_jogador_atual(estado) == 2))
         return 1;
-    else if ((obter_linha_ult(estado) == 7)&&(obter_coluna_ult(estado) == 7))
-        return 2;
-    else if (obter_jogador_atual(estado) == 1)
-        return 2;
     else
-        return 1;
+        return 2;
 }
 
 
 int encurralado(ESTADO *estado){
-    int x = obter_coluna_ult(estado);
-    int y = obter_linha_ult(estado);
-    int i1 = y-1;
-    int i2;
-    while (i1 <= y+1){
-        i2 = x-1;
-        while (i2 <= x + 1){
-            if (obter_casa(estado, i1, i2) == VAZIO)
-                return 0;
-            i2++;
-        }
-        i1++;
-    }
-    return 1;
+    COORDENADA c;
+    c = obter_coord_ult(estado);
+    LISTA l;
+    l = casas_disp(estado,c);
+    if(lista_esta_vazia(l)||(lista_esta_vazia(proximo(l))))
+        return 1;
+    else
+        return 0;
 }
 
 int e_vizinho (COORDENADA c1, COORDENADA c2){
@@ -74,9 +66,27 @@ int e_peca (COORDENADA c){
         return 0;
 }
 
+CASA qualcasa (char c){
+    if (c == '#')
+        return PRETA;
+    else if (c == '*')
+        return BRANCA;
+    else if (c == '2')
+        return DOIS;
+    else if (c == '1')
+        return UM;
+    else
+        return VAZIO;
+}
+
+int vencer(ESTADO *estado){
+    if (((obter_linha_ult(estado) == 0)&&(obter_coluna_ult(estado) == 0)) || ((obter_linha_ult(estado) == 7)&&(obter_coluna_ult(estado) == 7)) || (encurralado(estado)))
+        return 0;
+    else
+        return 1;
+}
 
 int jogar(ESTADO *e, COORDENADA c) {
-    printf("jogar %d %d\n", c.coluna, c.linha);
     int xu;
     int yu;
     xu = obter_coluna_ult(e);
@@ -100,7 +110,7 @@ int jogar(ESTADO *e, COORDENADA c) {
         guarda_ultima_jog(e,l,co);
     }
     else{
-        printf("Jogada Invalida\n");
+        erros(1);
         alteracasa(e,xu,yu,BRANCA);
     }
     int nj;
@@ -111,46 +121,6 @@ int jogar(ESTADO *e, COORDENADA c) {
         nj++;
     }
     return 1;
-}
-
-int vencer(ESTADO *estado){
-    if ((obter_linha_ult(estado) == 0)&&(obter_coluna_ult(estado) == 0))
-        return 0;
-    else if ((obter_linha_ult(estado) == 7)&&(obter_coluna_ult(estado) == 7))
-        return 0;
-    else if (encurralado(estado))
-        return 0;
-    else
-        return 1;
-}
-
-int write_coord(COORDENADA coord){
-    int x;
-    int y;
-    x = coord.coluna;
-    y = coord.linha;
-    char xc;
-    char yc;
-    xc = conv_c(x);
-    yc = conv_l(y);
-    printf("%c%c ",xc,yc);
-    return 0;
-}
-
-int movs(ESTADO *e){
-    int i = 0;
-    while (i < (obter_numero_de_jogadas(e)-1)){
-        printf("j%d: ",(i+1));
-        write_coord(obter_coord(e, 1, i));
-        write_coord(obter_coord(e, 2, i));
-        printf("\n");
-        i++;
-    }
-    if (obter_jogador_atual(e) == 2) {
-        printf("j%d: ",i+1);
-        write_coord(obter_coord(e, 1, i));
-    }
-    return 0;
 }
 
 ESTADO * reset_tab (ESTADO *e){
@@ -180,7 +150,7 @@ int *desenha_jogada(ESTADO *e, JOGADA jogada){
 }
 
 
-ESTADO *pos (ESTADO *e, int i){
+void pos (ESTADO *e, int i){
     int in = 0;
     JOGADA play;
     reset_tab(e);
@@ -214,9 +184,7 @@ ESTADO *pos (ESTADO *e, int i){
     }
 }
 
-LISTA casas_disp(ESTADO *e){
-    COORDENADA c;
-    c = obter_coord_ult(e);
+LISTA casas_disp(ESTADO *e,COORDENADA c) {
     LISTA l = criar_lista();
     COORDENADA *c1 = malloc(sizeof(NODO));
     COORDENADA *c2 = malloc(sizeof(NODO));
@@ -226,38 +194,22 @@ LISTA casas_disp(ESTADO *e){
     COORDENADA *c6 = malloc(sizeof(NODO));
     COORDENADA *c7 = malloc(sizeof(NODO));
     COORDENADA *c8 = malloc(sizeof(NODO));
-    c1->coluna = c.coluna+1;
-    c1->linha = c.linha+1;
-    c2->coluna = c.coluna+1;
-    c2->linha = c.linha;
-    c3->coluna = c.coluna+1;
-    c3->linha = c.linha-1;
-    c4->coluna = c.coluna;
-    c4->linha = c.linha+1;
-    c5->coluna = c.coluna;
-    c5->linha = c.linha-1;
-    c6->coluna = c.coluna-1;
-    c6->linha = c.linha+1;
-    c7->coluna = c.coluna-1;
-    c7->linha = c.linha;
-    c8->coluna = c.coluna-1;
-    c8->linha = c.linha-1;
-    if (e_peca(*c1)&&(e_vazio(*c1,e)))
-        l = insere_cabeca(l,c1);
-    if (e_peca(*c2)&&(e_vazio(*c2,e)))
-        l = insere_cabeca(l,c2);
-    if (e_peca(*c3)&&(e_vazio(*c3,e)))
-        l = insere_cabeca(l,c3);
-    if (e_peca(*c4)&&(e_vazio(*c4,e)))
-        l = insere_cabeca(l,c4);
-    if (e_peca(*c5)&&(e_vazio(*c5,e)))
-        l = insere_cabeca(l,c5);
-    if (e_peca(*c6)&&(e_vazio(*c6,e)))
-        l = insere_cabeca(l,c6);
-    if (e_peca(*c7)&&(e_vazio(*c7,e)))
-        l = insere_cabeca(l,c7);
-    if (e_peca(*c8)&&(e_vazio(*c8,e)))
-        l = insere_cabeca(l,c8);
+    c1->coluna = c.coluna + 1; c1->linha = c.linha + 1;
+    c2->coluna = c.coluna + 1; c2->linha = c.linha;
+    c3->coluna = c.coluna + 1; c3->linha = c.linha - 1;
+    c4->coluna = c.coluna; c4->linha = c.linha + 1;
+    c5->coluna = c.coluna; c5->linha = c.linha - 1;
+    c6->coluna = c.coluna - 1; c6->linha = c.linha + 1;
+    c7->coluna = c.coluna - 1; c7->linha = c.linha;
+    c8->coluna = c.coluna - 1; c8->linha = c.linha - 1;
+    if (e_peca(*c1)&&(e_vazio(*c1,e))) l = insere_cabeca(l, c1);
+    if (e_peca(*c2)&&(e_vazio(*c2,e))) l = insere_cabeca(l, c2);
+    if (e_peca(*c3)&&(e_vazio(*c3,e))) l = insere_cabeca(l, c3);
+    if (e_peca(*c4)&&(e_vazio(*c4,e))) l = insere_cabeca(l, c4);
+    if (e_peca(*c5)&&(e_vazio(*c5,e))) l = insere_cabeca(l, c5);
+    if (e_peca(*c6)&&(e_vazio(*c6,e))) l = insere_cabeca(l, c6);
+    if (e_peca(*c7)&&(e_vazio(*c7,e))) l = insere_cabeca(l, c7);
+    if (e_peca(*c8)&&(e_vazio(*c8,e))) l = insere_cabeca(l, c8);
     return l;
 }
 
@@ -265,9 +217,7 @@ COORDENADA rand_coord(LISTA l){
     srand((unsigned)time(NULL));
     int n;
     n = (rand() %7);
-    //printf("%d\n",n);
     while ((proximo(l)) && (n > 0)){
-        //printf("%d Eureka\n", n);
         l = remove_cabeca(l);
         n--;
     }
@@ -278,17 +228,17 @@ COORDENADA rand_coord(LISTA l){
 }
 
 int jogs(ESTADO *e){
-    jogar(e,rand_coord(casas_disp(e)));
+    jogar(e,rand_coord(casas_disp(e,obter_coord_ult(e))));
     return 0;
 }
 
 double distancia(COORDENADA c1, COORDENADA c2){
-    double r;
+    double r = 0;
     int x1 = c1.coluna;
     int x2 = c2.coluna ;
     int y1 = c1.linha;
     int y2 = c1.linha;
-    r = abs(x1 - x2) + abs(y1 - y2);
+    r = fabs(pow((double)(x1 - x2),2)+pow((double)(y1 - y2),2));
     return r;
 }
 
@@ -298,46 +248,19 @@ COORDENADA euclidiana(LISTA l, int player){
     COORDENADA cabeca;
     casa1.coluna = 0;
     casa1.linha = 0;
-    COORDENADA casa2;
-    casa2.coluna = 7;
-    casa2.linha = 7;
     c = *(COORDENADA *)devolve_cabeca(l);
     while (!lista_esta_vazia(l) && !lista_esta_vazia(proximo(l))){
         cabeca = *(COORDENADA *)(devolve_cabeca(l));
         if(player == 1){
-            if(distancia(cabeca,casa1) < distancia(c,casa1)){
-                c = cabeca;
-            }
-        }
-        else{
-            if(distancia(cabeca,casa2) < distancia(c,casa2)){
-                c = cabeca;
-            }
-        }
-        l = proximo(l);
+            if(distancia(cabeca,casa1) < distancia(c,casa1)) c = cabeca;}
+        else
+            if(distancia(cabeca,casa1) >= distancia(c,casa1)) c = cabeca;
+        l = remove_cabeca(l);
     }
     return c;
 }
 
 int jogs2(ESTADO *e){
-    jogar(e,euclidiana(casas_disp(e),obter_jogador_atual(e)));
+    jogar(e,euclidiana(casas_disp(e,obter_coord_ult(e)),obter_jogador_atual(e)));
     return 0;
-}
-
-COORDENADA altera_coord(COORDENADA c, int x, int y){
-    c.coluna = c.coluna + x;
-    c.linha = c.linha + y;
-    return c;
-}
-
-COORDENADA flood(ESTADO *e){
-    int flood[8][8];
-    int x;
-    int y;
-
-}
-
-int floodaux(ESTADO *es, int flood[8][8], COORDENADA c, int val){
-    
-
 }
